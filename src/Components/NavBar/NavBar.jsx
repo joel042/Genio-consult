3eae47b53693cc0658d98a874a150d8c058022cd
joel042 <uchennajoeleze@gmail.com>
2024-07-17 09:31:11 +0100

    .

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <>
+    <div>
+      <h1 className='text-blue-600'>this is the navbar</h1>
+    </div>
+    </>
+   
+  )
+}
+
+export default NavBar

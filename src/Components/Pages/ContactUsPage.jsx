2bdfc097c5c83f213f3e0b74da6b1dfe47622d86
black-myke <michaelbolajoko@gmail.com>
2024-07-30 03:06:53 +0100

    Contact Us Page

--- src/Components/Pages/ContactUsPage.jsx
+++ src/Components/Pages/ContactUsPage.jsx
@@ -10,14 +10,12 @@ const ContactUsPage = () => {
           <h1>Contact us</h1>
           <p>
             Let's talk about the Growth of your Business. Send us a message and
-            we will be in touch within one business day
+            we will be in touch within one business day.
           </p>
         </div>
       </div>
 
       <ContactUS />
-
-      {/*Contact-Us Hero Section */}
     </div>
   );
 };
